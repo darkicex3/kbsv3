a0b0525f65f973b091719ac27fc8bec00abd45f5
darkicex3 <maxime.junique@gmail.com>
2016-06-15 11:51:44 +0800

    first commit

--- kbsv3/settings.py
+++ kbsv3/settings.py
@@ -56,7 +56,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = 0
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_SSL_REDIRECT = False
 X_FRAME_OPTIONS = 'DENY'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '104.199.154.36', 'cuscom.xyz', '.cuscom.xyz']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
