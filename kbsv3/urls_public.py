218da4b3c6e6572fe27d6db15a7eb454d58ff434
darkicex3 <maxime.junique@gmail.com>
2016-06-17 17:23:38 +0800

    TEST WHITENOISE

--- kbsv3/urls_public.py
+++ kbsv3/urls_public.py
@@ -9,6 +9,5 @@ from . import settings
 
 urlpatterns = [
     url(r'^admin$', admin.site.urls),
-    url(r'^site_media/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^$', HomeView.as_view()),
 ]
