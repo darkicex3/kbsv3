87b03cae0827376b5844f130d4b2be13e99e11a0
darkicex3 <maxime.junique@gmail.com>
2016-06-17 15:04:58 +0800

    first commit

--- kbsv3/urls_public.py
+++ kbsv3/urls_public.py
@@ -4,6 +4,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view()),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view()),
 ]
